1298d2c04b01e5fcfac86834e2ed9b4978be1ef6
sonam <sonamsahu199518@gmail.com>
2022-04-19 21:43:39 +0530

    Add project files.

---
+++ Fibbo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibbo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 1;
+            int k;
+            Console.WriteLine("enter the position");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.Write(i+","+j+",");
+           
+            for(int a=2; a<k; a++ )
+            {
+                Console.Write(",");
+                int b = i + j;
+                i = j;
+                j = b;
+                Console.Write(b);
+               
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+         }
+    } 
+}
